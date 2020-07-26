2c6c633702ecdf9235418015b0813b67d2db4296
Joshua Arber <arber@unlv.nevada.edu>
2020-07-26 12:40:15 -0700

    added css styling; fixed equals operator

--- kalkulator_script.js
+++ kalkulator_script.js
@@ -1,63 +1,77 @@
-const buttons = document.querySelector('.calc-container');
 const digits = 10;
+const buttons = document.querySelector('.calc-container');
+buttons.style.display = 'flex';
+buttons.style.flexDirection = 'column';
+buttons.style.alignItems = 'center';
 
 var display = document.createElement('div');
-display.style.width = '300px';
+display.id = 'display';
+display.style.width = '230px';
 display.style.height = '100px';
+display.style.marginTop = '50px';
 display.style.borderStyle = 'solid';
 display.style.borderColor = 'black';
-display.style.borderWidth = '2px';
-display.style.fontSize = '60px';
+display.style.borderWidth = '5px';
+display.style.borderTopLeftRadius = '0.5em';
+display.style.borderTopRightRadius = '0.5em';
+display.style.fontFamily = 'helvetica';
+display.style.fontSize = '50px';
 display.style.display = 'flex';
 display.style.justifyContent = 'right';
-display.style.fontSize = '48px';
+display.style.padding = '5px';
 buttons.appendChild(display);
 
 var pad = document.createElement('div');
-pad.style.width = '300px';
-pad.style.height = '300px';
+pad.id = 'pad';
+pad.style.width = '230px';
+pad.style.height = '270px';
 pad.style.borderStyle = 'solid';
 pad.style.borderColor = 'black';
-pad.style.borderWidth = '2px';
-pad.style.display = 'flex';
-pad.style.justifyContent = 'space-evenly';
-pad.style.flexDirection = 'column';
+pad.style.borderWidth = '5px';
+pad.style.borderTopWidth = '1px';
+pad.style.borderBottomLeftRadius = '1.5em';
+pad.style.borderBottomRightRadius = '1.5em';
+pad.style.backgroundColor = '#c2c2c2';
+pad.style.display = 'grid';
+pad.style.alignItems = 'center';
+pad.style.justifyItems = 'center';
+pad.style.gridTemplateColumns = '1fr 1fr 1fr 1fr';
+pad.style.gridTemplateRows = '1fr 1fr 1fr 1fr';
+pad.style.padding = '5px';
 
-var row1 = document.createElement('div');
-var clear = document.createElement('button');
-styleButton(clear, 'c', row1);
-var div = document.createElement('button');
-styleButton(div, '/', row1);
-
-var row2 = document.createElement('div');
+// row 1
 for (i = 7; i < digits; i++) {
     var digitBtn = document.createElement('button');
-    styleButton(digitBtn, i, row2);         
+    styleButton(digitBtn, i);
 }
-var mul = document.createElement('button');
-styleButton(mul, '*', row2);
+var div = document.createElement('button');
+styleButton(div, '÷');
 
-var row3 = document.createElement('div');
+// row 2
 for (i = 4; i < 7; i++) {
     var digitBtn = document.createElement('button');
-    styleButton(digitBtn, i, row3);         
+    styleButton(digitBtn, i);
 }
-var minus = document.createElement('button');
-styleButton(minus, '-', row3);
+var mul = document.createElement('button');
+styleButton(mul, 'x');
 
-var row4 = document.createElement('div');
+// row 3
 for (i = 1; i < 4; i++) {
     var digitBtn = document.createElement('button');
-    styleButton(digitBtn, i, row4);
+    styleButton(digitBtn, i);
 }
-var plus = document.createElement('button');
-styleButton(plus, '+', row4);
+var minus = document.createElement('button');
+styleButton(minus, '-');
 
-var row5 = document.createElement('div');
+// row 4
+var clear = document.createElement('button');
+styleButton(clear, 'C');
 var digitBtn = document.createElement('button');
-styleButton(digitBtn, 0, row5);
+styleButton(digitBtn, 0);
 var equals = document.createElement('button');
-styleButton(equals, '=', row5);
+styleButton(equals, '=');
+var plus = document.createElement('button');
+styleButton(plus, '+');
 
 buttons.appendChild(pad);
 
@@ -109,11 +123,16 @@ class Stack {
 var exprStack = new Stack();
 var term = '';
 
-function styleButton(btn, label, row) {
-    btn.style.width = '50px';
-    btn.style.height = '50px';
-    btn.style.borderRadius = '50%';
-    btn.style.fontSize = '125%';    
+function styleButton(btn, label) {
+    btn.id = label;
+    btn.style.width = '45px';
+    btn.style.height = '45px';
+    btn.style.border = 'none';
+    btn.style.borderRadius = '37%'
+    btn.style.backgroundColor = 'black';
+    btn.style.color = 'white';
+    btn.style.fontSize = '110%';
+    btn.style.fontFamily = 'helvetica';
     btn.textContent = label;
     btn.value = label;
     btn.addEventListener('click', function() {
@@ -122,20 +141,22 @@ function styleButton(btn, label, row) {
             display.textContent = term;
         }
         else {
-            if (btn.value == 'c') {
+            if (btn.value == 'C') {
                 exprStack = new Stack();
                 term = '';
                 display.textContent = term;
             }
             else if (btn.value == '=') {
-                exprStack.push(term);
+                if (!isNaN(term) && !isNaN(exprStack.peek())) {
+                    exprStack.push(term);
 
-                var t1 = exprStack.pop();
-                var t2 = exprStack.pop();
-                var op = exprStack.pop();
+                    var t1 = exprStack.pop();
+                    var t2 = exprStack.pop();
+                    var op = exprStack.pop();
 
-                term = calculate(op, t1, t2);
-                display.textContent = term;
+                    term = calculate(op, t1, t2);
+                    display.textContent = term;
+                }
             }
             else {
                 if (!isNaN(term) && !isNaN(exprStack.peek())) {
@@ -146,7 +167,13 @@ function styleButton(btn, label, row) {
                     term = calculate(op, t1, t2);
                 }
 
-                exprStack.push(btn.value);
+                if (btn.value === '÷')
+                    exprStack.push('/');
+                else if (btn.value === 'x')
+                    exprStack.push('*');
+                else
+                    exprStack.push(btn.value);
+
                 exprStack.push(term);
                 display.textContent = term;
                 term = '';
@@ -156,9 +183,18 @@ function styleButton(btn, label, row) {
         console.log("term: " + term);
         console.log("stack: " + exprStack.printStack());
     });
+    
+    if (label == '÷' || label == 'x' || label == '-' || label == '+' || label == '=') {
+        btn.className = 'operator';
+    }
+    else if (label == 'C') {
+        btn.className = 'clear';
+    }
+    else {
+        btn.className = 'digit';
+    }
 
-    row.appendChild(btn);
-    pad.appendChild(row);
+    pad.appendChild(btn);
 }
 
 function calculate(op, t1, t2) {
@@ -187,7 +223,7 @@ function add(t1, t2) {
 }
 
 function subtract(t1, t2) {
-    return t1 - t2;
+    return -(t1 - t2);
 }
 
 function multiply(t1, t2) {
